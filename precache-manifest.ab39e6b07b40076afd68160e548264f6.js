f65dd17a2e4b3b322e85fc36e7e7ef843cd0fb34
Christopher Joshy <144864811+ChristopherJoshy@users.noreply.github.com>
2024-10-08 13:33:06 +0000

    Updates

---
+++ precache-manifest.ab39e6b07b40076afd68160e548264f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aead2aa227ef2803010b8dac1eb6cdd8",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "81102e60ef3a95dd0aa1",
+    "url": "/React-App/static/css/main.453b0a27.chunk.css"
+  },
+  {
+    "revision": "c8c6acff9b9680b43123",
+    "url": "/React-App/static/js/2.daf3f5a2.chunk.js"
+  },
+  {
+    "revision": "81102e60ef3a95dd0aa1",
+    "url": "/React-App/static/js/main.5c0c1f8a.chunk.js"
+  },
+  {
+    "revision": "0a1cf9ef188380b4f4c6",
+    "url": "/React-App/static/js/runtime~main.a4b1c3b3.js"
+  }
+]);
